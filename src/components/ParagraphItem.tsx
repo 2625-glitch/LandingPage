8698fbcdc7d5ba256962a1f50825e488f6d87607
2625-glitch <vennapureddypooji@gmail.com>
2024-04-02 13:56:01 +0530

    Added Pricing Component

---
+++ src/components/ParagraphItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// type ParagraphItemProps={
+//   info:stirng
+// }
+
+const ParagraphItem = ({ info, isActive }) => {
+  return (
+    <p className={`text-sm ${isActive ? "text-[#000000]" : "text-[#757575]"}`}>
+      {info}
+    </p>
+  );
+};
+
+export default ParagraphItem;
